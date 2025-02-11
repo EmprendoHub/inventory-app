3ef7deab9146d6bd694105fe0a165ea501c2ccd1
Salvador <54673042+EmprendoHub@users.noreply.github.com>
2025-02-11 16:44:11 -0600

    minor update

--- src/app/(backend)/sistema/ventas/pagos/page.tsx
+++ src/app/(backend)/sistema/ventas/pagos/page.tsx
@@ -13,11 +13,7 @@ export default async function Payments() {
 
   return (
     <div className="flex flex-col items-start justify-start bg-backgroundTwo p-4 rounded-md">
-      <BusinessHeader
-        title={"pagos"}
-        link={`negocio/pagos/nuevo`}
-        btn="Nuevo"
-      />
+      <BusinessHeader title={"pagos"} link={`ventas/pagos/nuevo`} btn="Nuevo" />
       <PaymentList payments={paymentsWithProductCount} />
     </div>
   );
